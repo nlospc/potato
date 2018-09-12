367e71e5c38360da542cc88812958f09a2273b3b
duanziqi <nlos-c@hotmail.com>
2018-09-12 15:35:29 +0800

    finish basis layout

---
+++ PotatoApp/app/src/main/java/com/example/nlospc/potatoapp/app/AppConst.java
@@ -0,0 +1,9 @@
+package com.example.nlospc.potatoapp.app;
+/**
+ * App全局变量*/
+public class AppConst {
+
+    public static final String BASE_URL="http://wanandroid.com/";
+    public static final String IS_LOGIN_KEY="isLogin";
+    public static final String USERNAME_KEY="userName";
+}
---
+++ PotatoApp/app/src/main/java/com/example/nlospc/potatoapp/model/ArticleBean.java
@@ -0,0 +1,8 @@
+package com.example.nlospc.potatoapp.model;
+
+/**
+ * Created by nlospc on 2018/9/12
+ * Description:
+ */
+public class ArticleBean {
+}
